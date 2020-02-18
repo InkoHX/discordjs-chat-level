fcbd433f1a7d068fde8e81127550e39b58e61eb6
InkoHX <inkohx@gmail.com>
2020-02-19 01:32:52 +0900

    Update Registry

--- src/registries/Registry.ts
+++ src/registries/Registry.ts
@@ -1,6 +1,7 @@
 import { Collection } from 'discord.js'
 import Client from '../Client'
 import util from 'util'
+import path from 'path'
 import glob, { IOptions } from 'glob'
 
 const globAsync = util.promisify(glob)
@@ -16,29 +17,46 @@ export default abstract class Registry<K, V> extends Collection<K, V> {
     this.client = client
   }
 
-  public abstract register (): Promise<V>
+  public abstract register(relativePath: string): Promise<V>
 
-  public abstract unregister (): Promise<V>
+  public unregister (key: K): V {
+    const value = this.get(key)
+    if (!value) throw new Error('There is no such key.')
+    this.delete(key)
 
-  public abstract registerAll (): Promise<V[]>
+    return value
+  }
 
-  public abstract unregisterAll(): Promise<V[]>
+  public async registerAll (pattern: string, options?: IOptions): Promise<V[]> {
+    const files = await this.scanFiles(pattern, options)
+
+    return Promise.all(files.map((file) => this.register(file)))
+  }
+
+  public unregisterAll (): Promise<V[]> {
+    return Promise.all(this.keyArray().map((key) => this.unregister(key)))
+  }
 
   protected scanFiles (pattern: string, options?: IOptions): Promise<string[]> {
     return globAsync(pattern, options)
   }
 
-  protected async loadModule (filePath: string): Promise<unknown> {
-    const Module: unknown = await import(filePath)
-      .then(value => value.default || value)
-      .catch(e => this.client.emit('error', e))
+  protected async loadModule (relativePath: string): Promise<unknown> {
+    try {
+      const absolutePath = path.join(process.cwd(), relativePath)
+      const Module: unknown = await import(absolutePath)
+        .then(value => value.default || value)
 
-    delete require.cache[filePath]
-    module.children.pop()
+      delete require.cache[absolutePath]
+      module.children.pop()
 
-    if (!this.isClassSyntax(Module)) throw new Error('Please use the ECMAScript 2015 class syntax.')
+      if (!this.isClassSyntax(Module)) throw new Error('Please use the ECMAScript 2015 class syntax.')
 
-    return new Module(this.client)
+      return new Module(this.client)
+    } catch (error) {
+      this.client.emit('error', error)
+      process.exit(1)
+    }
   }
 
   private isClassSyntax (arg: unknown): arg is Class<unknown> {
