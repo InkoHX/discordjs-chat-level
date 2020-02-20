49d781b94bac07d7e4e770efbf22d395c3cdbc1f
InkoHX <inkohx@gmail.com>
2020-02-20 11:01:46 +0900

    Add disabledEvents

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,10 @@
 import { Client } from '.'
 
-const bot = new Client()
+const bot = new Client({
+  disabledEvents: [
+    'TYPING_START',
+    'WEBHOOKS_UPDATE'
+  ]
+})
 
 bot.login().catch(console.error)
